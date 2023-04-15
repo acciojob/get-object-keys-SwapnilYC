5f830725a2c3cebd5ddab68feaaa2307126a710d
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-15 13:05:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = { name: "swapnil" };
+
+
+Object.prototype.getKeys = function () {
+  let arr = [];
+  for (let [key, val] of Object.entries(this)) {
+    arr.push(key)
+    // console.log(key)
+  }
+  return arr;
+}
